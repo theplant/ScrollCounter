ec41e32fd6f9f372dbc80ece115fb22ef4f8ee9e
WN <helen@theplant.jp>
2025-01-08 13:31:02 +0900

    add path

--- Package.swift
+++ Package.swift
@@ -15,6 +15,7 @@ let package = Package(
     targets: [
         .target(
             name: "ScrollCounter",
+            path: "ScrollCounter",
             dependencies: []
         ),
         .testTarget(
