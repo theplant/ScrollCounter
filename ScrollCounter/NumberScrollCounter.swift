58f70dc2409c8bf44020b40b56c1d4966b41fdb0
WN <helen@theplant.jp>
2025-01-08 11:38:27 +0900

    some modify

--- ScrollCounter/NumberScrollCounter.swift
+++ ScrollCounter/NumberScrollCounter.swift
@@ -23,21 +23,23 @@ public class NumberScrollCounter: UIView {
         return slideDuration / 2
     }
     /// The animation duration used when a `DigitScrollCounter` is scrolling to a number.
-    public var scrollDuration: TimeInterval
+    public var scrollDuration: TimeInterval = 0.33
     /// The animation duration when the items in `digitScrollers` are slid to their new origins.
     public var slideDuration: TimeInterval = 0.5
     
     /// The current value being displayed, or the number being animated to if the `NumberScrollCounter` is still animating.
-    public private(set) var currentValue: Float
+    public private(set) var currentValue: Float = 0
     
     /// The spacing between the `seperator` and the adjacent items in `digitScrollers`.
-    public var seperatorSpacing: CGFloat
+    public var seperatorSpacing: CGFloat = 0
     /// The number of decimal places that should be displayed.
-    public var decimalPlaces: Int
+    public var decimalPlaces: Int = 3
+    /// Digital thousands separated.
+    public var decimalSeperatorCount: Int = 0
     /// The font to use for all of the labels used in building the `NumberScrollCounter`.
-    public let font: UIFont
+    public var font: UIFont =  UIFont(name: "SpeedeeApp-Bold", size: 41)!
     /// The text color to use for all of the labels used in building the `NumberScrollCounter`.
-    public let textColor: UIColor
+    public var textColor: UIColor = H.c(0x292929)
     private let digitScrollerBackgroundColor: UIColor = .clear
     
     /// The string to use as a prefix to the items in `digitScrollers`.
@@ -45,7 +47,7 @@ public class NumberScrollCounter: UIView {
     /// The string to use as a suffix to the items in `digitScrollers`.
     public var suffix: String?
     /// The string to use as the decimal indicator for the items in `digitScrollers`.
-    let seperator: String
+    var seperator: String = ","
     /// The string that will be used to represent negative values.
     let negativeSign = "-"
     
@@ -54,12 +56,12 @@ public class NumberScrollCounter: UIView {
     /// The view that holds the suffix, or `nil` if there is no suffix.
     private var suffixView: UIView?
     /// The view that holds the seperator, or `nil` if there is no seperator.
-    private var seperatorView: UIView?
+    private var seperatorViews: [UIView]?
     /// The view that holds the negative sign, or `nil` if the number is not negative.
     private var negativeSignView: UIView?
     
-    private let gradientColor: UIColor?
-    private let gradientStop: Float?
+    private var gradientColor: UIColor? = nil
+    private var gradientStop: Float? = 0.2
     
     /// The animator controlling the current animation in the ScrollableCounter.
     private var animator: UIViewPropertyAnimator?
@@ -86,69 +88,27 @@ public class NumberScrollCounter: UIView {
         )
     }
     
-    // MARK: - Init
-    
-    /**
-     Initialize a `NumberScrollCounter` with the given parameters.
-     
-     After the view is initializes, it's digit scrollers will be set to the given values, and the frame will be resized using `sizeToFit()`.
-     
-     - note:
-     To get a design similar to Robinhood, try using `"Avenir-Black"` as the font.
-     
-     - parameters:
-        - value: The initial value to display.
-        - scrollDuration: The duration that is used when animating a single digit's scrolling animation.  Defaults to `0.3`.
-        - decimalPlaces: The number of decimals to display.  Defaults to `0`.
-        - prefix: The prefix to use in front of the displayed number.  Defaults to `nil`, which results in no prefix.
-        - suffix: The suffix to use at the end of the displayed number.  Defaults to `nil`, which results in no suffix.
-        - seperator: The seperator to use to represent a decimal.  Defaults to `"."`.
-        - seperatorSpacing: The spacing to use between the seperator and the adjacent digits.  Defaults to `5`.
-        - font: The font to use for the digits, prefix, suffix, and seperator.
-        - textColor: The text color to use for the digits, prefix, suffix, and seperator.
-        - animateInitialValue: Whether or not the initial value should be animated to. Defaults to `false`.
-        - gradientColor: The color to use for the vertical gradient.  If this is `nil`, then no gradient is applied.
-        - gradientStop: The stopping point for the gradient, where the bottom stopping point is (1 - gradientStop).  If gradientStop is not less than 0.5 than it is ignored.  If this is `nil`, then no gradient is applied.
-     */
-    public init(value: Float, scrollDuration: TimeInterval = 0.3, decimalPlaces: Int = 0, prefix: String? = nil, suffix: String? = nil, seperator: String = ".", seperatorSpacing: CGFloat = 0, font: UIFont = UIFont.boldSystemFont(ofSize: UIFont.labelFontSize), textColor: UIColor = .black, animateInitialValue: Bool = false, gradientColor: UIColor? = nil, gradientStop: Float? = nil) {
-
-        self.currentValue = value
-        
-        self.decimalPlaces = decimalPlaces
-        self.font = font
-        self.textColor = textColor
-        
-        self.prefix = prefix
-        self.suffix = suffix
-        self.seperator = seperator
-        self.seperatorSpacing = seperatorSpacing
-        
-        self.scrollDuration = scrollDuration
-        self.gradientColor = gradientColor
-        if let stoppingPoint = gradientStop, stoppingPoint < 0.5 {
-            self.gradientStop = gradientStop
-        } else {
-            self.gradientStop = nil
-        }
-        
-        super.init(frame: CGRect.zero)
-        
-        setValue(value, animated: animateInitialValue)
+    private func initializeConfig() {
+        setValue(self.currentValue, animated: false)
         frame.size.height = digitScrollers.first!.height
-        
         sizeToFit()
     }
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initializeConfig()
+        
+    }
+    
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+        initializeConfig()
+        
     }
     
     public override func sizeToFit() {
         var width: CGFloat = 0
-        
-        if let suffixView = suffixView {
-            width = suffixView.frame.origin.x + suffixView.frame.width
-        } else if let lastDigit = digitScrollers.last {
+        if let lastDigit = digitScrollers.last {
             width = lastDigit.frame.origin.x + lastDigit.frame.width
         }
         
@@ -200,14 +160,33 @@ public class NumberScrollCounter: UIView {
      - returns: An array of strings that matches the given value.
      */
     private func getStringArray(fromValue value: Float) -> [String] {
-        return String(format: "%.\(decimalPlaces)f", value).compactMap { character -> String in
-            var entry = String(character)
-            let result = character.wholeNumberValue
-            if let resultNumber = result {
-                entry = "\(resultNumber)"
-            }
-            return entry
+        if let formattedNumber = formatNumToString(fromValue: value) {
+            decimalSeperatorCount = formattedNumber.filter { $0 == "," }.count
+            let array =  Array(formattedNumber).map { String($0) }
+            return array
         }
+        return []
+        
+    }
+    
+    func getNumWidth() -> CGFloat {
+        if let lastDigit = digitScrollers.last {
+           let width = lastDigit.frame.origin.x + lastDigit.frame.width
+            return width
+        }
+        
+        return 0
+    }
+    
+    func formatNumToString(fromValue value: Float) -> String? {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal // Use decimal style for formatting
+        formatter.groupingSeparator = "," // Set the separator to comma (",")
+        formatter.groupingSize = 3
+        if let formattedNumber = formatter.string(from: NSNumber(value: value)) {
+            return formattedNumber
+        }
+        return nil
     }
         
     
@@ -243,7 +222,7 @@ public class NumberScrollCounter: UIView {
         updatePrefix()
         createSeperatorViewIfNeeded()
         updateDigitScrollersLayout()
-        updateSuffix()
+       // updateSuffix()
         
         animator!.addCompletion({ _ in
             self.animator = nil
@@ -260,22 +239,47 @@ public class NumberScrollCounter: UIView {
      Creates a seperator view if one is needed but does not exist.  This does not update the layout of the seperator view.
      */
     private func createSeperatorViewIfNeeded() {
-        guard decimalPlaces > 0, seperatorView == nil else {
-            return
+        if let views = seperatorViews {
+            views.forEach { view in
+                view.removeFromSuperview()
+            }
         }
         
-        let seperatorLabel = UILabel()
-        seperatorLabel.text = seperator
-        seperatorLabel.textColor = textColor
-        seperatorLabel.font = font
-        seperatorLabel.sizeToFit()
-        seperatorLabel.frame.size.width += 2 * seperatorSpacing
-        seperatorLabel.textAlignment = .center
-        seperatorLabel.frame.origin = CGPoint.zero
-        addSubview(seperatorLabel)
+        guard decimalSeperatorCount > 0 else {
+            return
+        }
+       
+        seperatorViews = nil
+        for _ in 1...decimalSeperatorCount {
+            let seperatorLabel = UILabel()
+            seperatorLabel.text = seperator
+            seperatorLabel.textColor = textColor
+            seperatorLabel.font = font
+            seperatorLabel.sizeToFit()
+            seperatorLabel.frame.size.width += 2 * seperatorSpacing
+            seperatorLabel.textAlignment = .center
+            seperatorLabel.frame.origin = CGPoint.zero
+            addSubview(seperatorLabel)
+            seperatorLabel.alpha = 0
+            if var views = seperatorViews {
+                views.append(seperatorLabel)
+                seperatorViews = views
+            } else {
+                seperatorViews = [seperatorLabel]
+            }
+        }
         
-        seperatorLabel.alpha = 0
-        seperatorView = seperatorLabel
+    }
+    
+    func getLocationNumber(startingFrom number: Int) -> [Int] {
+        var result: [Int] = []
+        var currentNumber = number - 3
+        while currentNumber > 0 {
+            result.append(currentNumber)
+            currentNumber -= 3
+        }
+        
+        return result.reversed()
     }
     
     /**
@@ -287,28 +291,28 @@ public class NumberScrollCounter: UIView {
         }
         
         let startingX = startingXCoordinate
-        let seperatorLocation = digitScrollers.count - decimalPlaces
-        
+        let seperatorLocations = getLocationNumber(startingFrom: digitScrollers.count)
         for (index, scroller) in digitScrollers.enumerated() {
             if scroller.superview == nil {
                 addSubview(scroller)
                 scroller.frame.origin.x = startingX
                 scroller.alpha = 0
             }
+            let seperatorLocationCount: CGFloat = CGFloat(seperatorLocations.filter({ index >= $0}).count)
             
             var x = startingX + CGFloat(index) * scroller.width
-            if index >= seperatorLocation, let seperatorView = seperatorView {
-                x += seperatorView.frame.width
+            if seperatorLocationCount > 0, let seperatorView = seperatorViews?[0] {
+                x += (seperatorView.frame.width * seperatorLocationCount)
             }
             animator.addAnimations {
                 scroller.alpha = 1
                 scroller.frame.origin.x = x
             }
             
-            if index == seperatorLocation, let seperatorView = seperatorView {
+            if let seperatorIndex = seperatorLocations.firstIndex(of: index), let seperatorView = seperatorViews?[seperatorIndex] {
                 animator.addAnimations {
                     seperatorView.alpha = 1
-                    seperatorView.frame.origin.x = (startingX + CGFloat(index) * scroller.width)
+                    seperatorView.frame.origin.x = (startingX + CGFloat(index) * scroller.width) + CGFloat(seperatorView.frame.width * CGFloat(seperatorIndex))
                 }
             }
         }
@@ -416,9 +420,9 @@ public class NumberScrollCounter: UIView {
         if let suffixView = self.suffixView, let scroller = digitScrollers.first {
             var suffixX: CGFloat = 0
             suffixX += scroller.frame.width * CGFloat(digitScrollers.count)
-            if let view = seperatorView {
-                suffixX += view.frame.width
-            }
+//            if let view = seperatorView {
+//                suffixX += view.frame.width
+//            }
             if let view = prefixView {
                 suffixX += view.frame.width
             }
